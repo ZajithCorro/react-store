fb453777c9f58eb995b373b51cd7a95fc6092ab6
Zajith Corro <zajithcorrov@hotmail.com>
2022-08-08 23:43:55 -0500

    :sparkles: [feat] Add the router for the page

---
+++ src/routes/index.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+// import Checkout from '../containers/Checkout';
+// import Information from '../containers/Information';
+// import Payment from '../containers/Payment';
+// import Success from '../containers/Success';
+// import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout';
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					{/* <Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/checkout/information" component={Information} />
+					<Route exact path="/checkout/payment" component={Payment} />
+					<Route exact path="/checkout/success" component={Success} />
+					<Route component={NotFound} /> */}
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
+};
+
+export default App;

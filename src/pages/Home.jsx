d4d16e8235f9d67b98e4d11eab51a62b4fbd32d9
Zajith Corro <zajithcorrov@hotmail.com>
2022-09-05 23:18:59 -0500

    :sparkles: [feat] Now use the platzi fake API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
+
 import ProductsList from '../components/ProductsList';
-import data from '../data';
 
 const Home = () => {
-	return <ProductsList products={data.products} />;
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		async function getData() {
+			const resp = await fetch(
+				'https://api.escuelajs.co/api/v1/products?offset=0&limit=10'
+			);
+			const list = await resp.json();
+
+			setProducts(list);
+		}
+
+		getData();
+	}, []);
+
+	return <ProductsList products={products} />;
 };
 
 export default Home;
